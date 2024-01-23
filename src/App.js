0df8c2ea977dc98fb7dfea8782d74b212ba9828d
Manuel26190 <manuelfaure26190@gmail.com>
2024-01-23 18:07:45 +0100

    save

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'; // Importez le crochet 'useState'
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState(null); // Utilisez l'état pour stocker les données
+
+  const handleAxiosClick = async () => {
+    try {
+      const response = await axios.get('https://swapi.dev/api/people/1/');
+      //console.log(response.data);
+      setData(response.data); // Mettez à jour l'état avec les données reçues
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  //console.log(data);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Api-test</h1>
+        <div>
+          <button className="btn-axios" onClick={handleAxiosClick}>
+            Appel Axios
+          </button>
+          <p>{data && <pre>{JSON.stringify(data, null, 2)}</pre>}</p>
+        </div>
       </header>
     </div>
   );
