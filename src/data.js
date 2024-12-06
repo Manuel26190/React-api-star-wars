11e8d385a623b53298315d971bbf1a697d1a4657
Manuel26190 <manuelfaure26190@gmail.com>
2024-12-06 10:30:29 +0100

    save luke sky img

--- src/data.js
+++ src/data.js
@@ -2,51 +2,61 @@ export const dataStarWars = [
     {
         id: 1,
         name: 'Luke Skywalker',
-        picture: 'https://www.ecranlarge.com/media/cache/1600x1200/uploads/image/001/006/star-wars-episode-iv-un-nouvel-espoir-la-guerre-des-etoiles-photo-mark-hamill-1006592.jpg',
+        picture:
+            'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/f584304d-836b-4b78-8efb-9c7294fe6940/dge13gk-5a045e8d-f8f4-4150-a95c-0db944184999.jpg/v1/fill/w_1280,h_1280,q_75,strp/younger_luke_skywalker_on_the_hunt_for_a_jedi_by_justinchauveaudesign_dge13gk-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTI4MCIsInBhdGgiOiJcL2ZcL2Y1ODQzMDRkLTgzNmItNGI3OC04ZWZiLTljNzI5NGZlNjk0MFwvZGdlMTNnay01YTA0NWU4ZC1mOGY0LTQxNTAtYTk1Yy0wZGI5NDQxODQ5OTkuanBnIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.Ke6ShXfxIWC8z2A4F8SMgl-DpWrLslJkjylimaBzQaw',
     },
     {
         id: 2,
         name: 'C-3PO',
-        picture: 'https://media.gqmagazine.fr/photos/5df255b9c6b0590008eb2bd9/16:9/w_2560%2Cc_limit/main.jpg',
+        picture:
+            'https://media.gqmagazine.fr/photos/5df255b9c6b0590008eb2bd9/16:9/w_2560%2Cc_limit/main.jpg',
     },
     {
         id: 3,
         name: 'R2-D2',
-        picture: 'https://www.derivstore.com/wp-content/uploads/2016/02/Figurines-C3PO-R2D2-2.jpg',
+        picture:
+            'https://www.derivstore.com/wp-content/uploads/2016/02/Figurines-C3PO-R2D2-2.jpg',
     },
     {
         id: 4,
         name: 'Darth Vader',
-        picture: 'https://www.tomsguide.fr/content/uploads/sites/2/2016/12/dark-vador-1024x767.jpg',
+        picture:
+            'https://www.tomsguide.fr/content/uploads/sites/2/2016/12/dark-vador-1024x767.jpg',
     },
     {
         id: 5,
         name: 'Leia Organa',
-        picture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfBxSXRxn-eF8aYNB-uqwFCVZ_jcv7dR10HFncH09cfCr5P2Ard4XzxA2K26iK7CY1QiU&usqp=CAU',
+        picture:
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfBxSXRxn-eF8aYNB-uqwFCVZ_jcv7dR10HFncH09cfCr5P2Ard4XzxA2K26iK7CY1QiU&usqp=CAU',
     },
     {
         id: 6,
         name: 'Owen Lars',
-        picture: 'https://www.starwarsnewsnet.com/wp-content/uploads/2017/06/Owen002.jpg',
+        picture:
+            'https://www.starwarsnewsnet.com/wp-content/uploads/2017/06/Owen002.jpg',
     },
     {
         id: 7,
         name: 'Beru Whitesun lars',
-        picture: 'https://clonecorridor.files.wordpress.com/2015/09/beru-lars-6.jpg',
+        picture:
+            'https://clonecorridor.files.wordpress.com/2015/09/beru-lars-6.jpg',
     },
     {
         id: 8,
         name: 'R5-D4',
-        picture: 'https://static.fnac-static.com/multimedia/Images/F9/5F/0D/15/22074873-3-1520-2/tsp20230613125055/Figurine-Hot-Toys-TMS086-Star-wars-The-Book-Of-Boba-Fett-R5-D4-Pit-Droid-BD-72.jpg',
+        picture:
+            'https://static.fnac-static.com/multimedia/Images/F9/5F/0D/15/22074873-3-1520-2/tsp20230613125055/Figurine-Hot-Toys-TMS086-Star-wars-The-Book-Of-Boba-Fett-R5-D4-Pit-Droid-BD-72.jpg',
     },
     {
         id: 9,
         name: 'Biggs Darklighter',
-        picture: 'https://lumiere-a.akamaihd.net/v1/images/image_606ff7f7.jpeg?region=0%2C0%2C1560%2C878',
+        picture:
+            'https://lumiere-a.akamaihd.net/v1/images/image_606ff7f7.jpeg?region=0%2C0%2C1560%2C878',
     },
     {
         id: 10,
         name: 'Obi-Wan Kenobi',
-        picture: 'https://media.vogue.fr/photos/606dca7e47f477da2ff11fcf/1:1/w_2032,h_2032,c_limit/010_A7A08C89_416.jpg',
+        picture:
+            'https://media.vogue.fr/photos/606dca7e47f477da2ff11fcf/1:1/w_2032,h_2032,c_limit/010_A7A08C89_416.jpg',
     },
 ]
